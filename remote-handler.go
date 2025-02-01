dee0dfd836ff71b220b51d1b58d85bef050c1a1b
matttm <matttmaloney@gmail.com>
2025-02-01 17:01:03 -0500

    implementing interface

--- remote-handler.go
+++ remote-handler.go
@@ -18,40 +18,32 @@ func NewRemoteHandler(c *client.Conn) ProxyRequestHandler {
 
 // UseDB is called for COM_INIT_DB
 func (h ProxyRequestHandler) UseDB(dbName string) error {
-	return h.UseDB(dbName)
+	log.Println("In UseDb")
+	return h.remoteDb.UseDB(dbName)
 }
 
 // HandleQuery is called for COM_QUERY
 func (h ProxyRequestHandler) HandleQuery(query string) (*mysql.Result, error) {
-	log.Printf("Received: Query: %s", query)
-
-	// These two queries are implemented for minimal support for MySQL Shell
-	if query == `SET NAMES 'utf8mb4';` {
-		return nil, nil
+	log.Println("In HandleQuery")
+	stmt, err := h.remoteDb.Prepare(query)
+	if err != nil {
+		return nil, err
 	}
-	if query == `select concat(@@version, ' ', @@version_comment)` {
-		r, err := mysql.BuildSimpleResultset([]string{"concat(@@version, ' ', @@version_comment)"}, [][]interface{}{
-			{"8.0.11"},
-		}, false)
-		if err != nil {
-			return nil, err
-		}
-		return mysql.NewResult(r), nil
-	}
-
-	return nil, fmt.Errorf("not supported now")
+	return stmt.Execute()
 }
 
 // HandleFieldList is called for COM_FIELD_LIST packets
 // Note that COM_FIELD_LIST has been deprecated since MySQL 5.7.11
 // https://dev.mysql.com/doc/dev/mysql-server/latest/page_protocol_com_field_list.html
 func (h ProxyRequestHandler) HandleFieldList(table string, fieldWildcard string) ([]*mysql.Field, error) {
-	return h.HandleFieldList(table, fieldWildcard)
+	log.Println("In HandleFieldList")
+	return nil, mysql.NewError(2, "not implemented")
 }
 
 // HandleStmtPrepare is called for COM_STMT_PREPARE
 func (h ProxyRequestHandler) HandleStmtPrepare(query string) (int, int, interface{}, error) {
-	return h.HandleStmtPrepare(query)
+	log.Println("In HandleStmtPrepare")
+	return 0, 0, nil, mysql.NewError(2, "not implemented")
 }
 
 // 'context' isn't used but replacing it with `_` would remove important information for who
@@ -60,12 +52,14 @@ func (h ProxyRequestHandler) HandleStmtPrepare(query string) (int, int, interfac
 
 // HandleStmtExecute is called for COM_STMT_EXECUTE
 func (h ProxyRequestHandler) HandleStmtExecute(context interface{}, query string, args []interface{}) (*mysql.Result, error) {
-	return h.HandleStmtExecute(context, query, args)
+	log.Println("In HandleStmtExecute")
+	return nil, mysql.NewError(2, "not implemented")
 }
 
 // HandleStmtClose is called for COM_STMT_CLOSE
 func (h ProxyRequestHandler) HandleStmtClose(context interface{}) error {
-	return h.HandleStmtClose(context)
+	log.Println("In HandleStmtClose")
+	return mysql.NewError(2, "not implemented")
 }
 
 // HandleOtherCommand is called for commands not handled elsewhere
