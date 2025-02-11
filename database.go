35d378df490b1bdf2b100c230460ef84e64a7c14
matttm <matttmaloney@gmail.com>
2025-02-11 17:37:02 -0500

    refactoring

--- database.go
+++ database.go
@@ -2,19 +2,36 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/dolthub/go-mysql-server/memory"
 	"github.com/dolthub/go-mysql-server/sql"
+	"github.com/go-mysql-org/go-mysql/client"
+	"github.com/go-mysql-org/go-mysql/mysql"
 )
 
 var (
 	dbName    = "mydb"
 	tableName = "mytable"
 	address   = "localhost"
+	port      = 3306
+	host      = os.Getenv("DB_HOST")
+	user      = os.Getenv("DB_USER")
+	pass      = os.Getenv("DB_PASS")
 )
 
 var ctx *sql.Context = nil
+
+func InitOverseerConnection() *client.Conn {
+	// create connection to ask user what should be imposed
+	conductor, err := client.Connect(fmt.Sprintf("%s:%d", host, port), user, pass, "")
+	if err != nil {
+		panic(err)
+	}
+	return conductor
+}
 
 func InitEmptyDatabase() *memory.DbProvider {
 	pro := createTestDatabase()
@@ -33,4 +50,32 @@ func createTestDatabase() *memory.DbProvider {
 	session := memory.NewSession(sql.NewBaseSession(), pro)
 	ctx = sql.NewContext(context.Background(), sql.WithSession(session))
 	return pro
+}
+
+func QueryForPropety(c *client.Conn, query string) []string {
+	r, err := c.Execute(SHOW_DB_QUERY)
+	if err != nil {
+		panic(err)
+	}
+	//
+	// Close result for reuse memory (it's not necessary but very useful)
+	defer r.Close()
+
+	// Handle resultset
+	// v, _ := r.GetInt(0, 0)
+	// v, _ = r.GetIntByName(0, "id")
+
+	// Direct access to fields
+	for _, row := range r.Values {
+		for _, val := range row {
+			// _ := val.Value() // interface{}
+			// or
+			if val.Type == mysql.FieldValueTypeString {
+				log.Print(string(val.Value().([]uint8)))
+			}
+		}
+	}
+
+	// lets see what schemas are available
+
 }
