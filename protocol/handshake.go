d4ca5caaa4c656b9ee72783f509d2918bf5cc34e
matttm <matttmaloney@gmail.com>
2025-03-13 15:21:51 -0400

    working on decoding test

--- protocol/handshake.go
+++ protocol/handshake.go
@@ -3,17 +3,19 @@ package protocol
 import (
 	"bytes"
 	"encoding/binary"
+	"fmt"
 )
 
+// Definition can be found at https://dev.mysql.com/doc/dev/mysql-server/8.4.3/page_protocol_basic_packets.html
 type PacketHeader struct {
 	Length     uint32
 	SequenceId uint8
 }
 
-// MySql Protocol::HandshakeV10
+// MySql Protocol::HandshakePacket
 //
 // Definition can be fond at: https://dev.mysql.com/doc/dev/mysql-server/latest/page_protocol_connection_phase_packets_protocol_handshake_v10.html
-type HandshakeV10 struct {
+type HandshakePacket struct {
 	Header              *PacketHeader
 	ProtocolVersion     uint8
 	ServerVersion       string
@@ -30,25 +32,31 @@ type HandshakeV10 struct {
 	AuthPluginName      string
 }
 
-func Decode(data []byte) (*HandshakeV10, error) {
-	payload := &HandshakeV10{}
-
-	// TODO: FIND DOC FOR THIS HEADER
-	_ = data[:4]
+func Decode(data []byte) (*HandshakePacket, error) {
+	payload := &HandshakePacket{}
+	header := &PacketHeader{}
+	var payloadLength uint32 = 0
+	payloadLength |= uint32(data[0]) | (uint32(data[1]) << 8) | (uint32(data[2]) << 16)
+	header.Length = payloadLength
+	header.SequenceId = data[3]
+	payload.Header = header
 
 	payload.ProtocolVersion = data[4]
-	data = data[5:] // this reassignment is done so the read bytes arnt included in search
 
+	data = data[5:] // this reassignment is done so the read bytes arnt included in search
+	fmt.Printf("right before serV %02x\n", data[0])
 	// this string is null terminated, sp look dfor null
 	serverVersionEndIdx := bytes.IndexByte(data, 0x00)
-	payload.ServerVersion = string(data[:serverVersionEndIdx+1]) // we add 1 so null byte is included
+	payload.ServerVersion = string(data[:serverVersionEndIdx])
 
-	data = data[:serverVersionEndIdx+2] // we add 2 so we move past null byte
+	data = data[serverVersionEndIdx+1:] // we add 1 so we move past null byte (nullbyte = data[serverVEIdx+1]
+	fmt.Printf("right after serV %02x\n", data[0])
 
 	buffer := bytes.NewReader(data)
 	if err := binary.Read(buffer, binary.LittleEndian, &payload.ThreadID); err != nil {
 		return payload, err
 	}
+	fmt.Printf("%02x", payload.ThreadID)
 
 	if err := binary.Read(buffer, binary.LittleEndian, &payload.AuthPluginDataPart1); err != nil {
 		return payload, err
