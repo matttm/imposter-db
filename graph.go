6b5b85f58f6e94afa123f5e5a3499bde0de08193
matttm <matttmaloney@gmail.com>
2025-06-30 20:42:57 -0400

    starting topo

--- graph.go
+++ graph.go
@@ -2,4 +2,14 @@ package main
 
 func topologicalSort(tables [][2]string) []string {
 	var adj map[string][]string
+	for _, edge := range tables {
+		p := edge[0]
+		c := edge[1]
+		adj[p] = append(adj[p], c)
+	}
+	topoSorted := []string{}
+	return topoSorted
+}
+
+func dfs(adj map[string][]string, topo []string, curr string) {
 }
