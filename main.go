07c04dde9453bcee25b96350c9e22cc38089f4d5
matttm <matttmaloney@gmail.com>
2025-07-13 14:07:12 -0400

    adding check

--- main.go
+++ main.go
@@ -36,16 +36,17 @@ func handleConn(c net.Conn, schema, tableName string) {
 }
 func main() {
 	s := selection{}
-	schemaFlag := flag.String("schema", "", "a string")
-	tableFlag := flag.String("table", "", "a string")
+	schemaFlag := flag.String("schema", "", "a string of the schema name")
+	tableFlag := flag.String("table", "", "a string of the table name")
+	fkFlag := flag.Bool("fk", false, "a boolean")
 	flag.Parse()
 
 	remoteDb := InitRemoteConnection()
 	defer remoteDb.Close()
 	log.Println("Remote database init")
-	// localDb := InitLocalDatabase()
-	// defer localDb.Close()
-	// log.Println("Local database init")
+	localDb := InitLocalDatabase()
+	defer localDb.Close()
+	log.Println("Local database init")
 
 	log.Printf("Checking for available databases...")
 	databases := QueryFor(remoteDb, SHOW_DB_QUERY)
@@ -53,6 +54,9 @@ func main() {
 		s.databases = PromptSelection("Choose database", databases)
 		if len(s.databases) < 1 {
 			log.Panic("Error: no selection made")
+		}
+		if len(s.databases) > 1 {
+			log.Panic("Error: one selection is currently supported")
 		}
 	} else {
 		s.databases = []string{*schemaFlag}
@@ -65,69 +69,74 @@ func main() {
 		if len(s.tables) < 1 {
 			log.Panic("Error: no selection made")
 		}
+		if len(s.tables) > 1 {
+			log.Panic("Error: one selection is currently supported")
+		}
 	} else {
 		s.tables = []string{*tableFlag}
 	}
 	log.Printf("You chose %s", s.tables[0])
 
-	// ReplaceDB(localDb, s.databases[0])
+	ReplaceDB(localDb, s.databases[0])
 
-	// create all referencing tables in localDb
-	foreignTables := QueryForTwoColumns(remoteDb, FETCH_GRAPH_EDGES(s.databases[0], s.tables[0])) // columns[0][0] should be primary key
+	if *fkFlag {
+		// create all referencing tables in localDb
+		foreignTables := QueryForTwoColumns(remoteDb, FETCH_GRAPH_EDGES(s.databases[0], s.tables[0])) // columns[0][0] should be primary key
 
-	// getting heirarchical ordering
-	inverseTopologicalOrdering, _ := topologicalSort(foreignTables)
-	// TODO: move this code to manip service
-	for i, v := range inverseTopologicalOrdering {
-		inverseTopologicalOrdering[i] = fmt.Sprintf("'%s'", v)
+		// getting heirarchical ordering
+		inverseTopologicalOrdering, _ := topologicalSort(foreignTables)
+		// TODO: move this code to manip service
+		for i, v := range inverseTopologicalOrdering {
+			inverseTopologicalOrdering[i] = fmt.Sprintf("'%s'", v)
+		}
+		topoString := strings.Join(inverseTopologicalOrdering, ",")
+		inParam := fmt.Sprintf("(%s)", topoString)
+		estimated := SelectOneDynamic(remoteDb, FETCH_TABLES_SIZES(s.databases[0], inParam))
+		MAX := 0.05
+		if *estimated > MAX {
+			log.Printf("Error: total tables size %f GB exceeds %f GB", *estimated, MAX)
+			log.Printf("Falling back to ignoring foreign keys")
+		} else {
+			log.Printf("Estimated replication size: %f", *estimated)
+			s.tables = []string{}
+			for _, tableName := range inverseTopologicalOrdering {
+				s.tables = append(s.tables, tableName)
+			}
+		}
 	}
-	topoString := strings.Join(inverseTopologicalOrdering, ",")
-	inParam := fmt.Sprintf("(%s)", topoString)
-	estimated := SelectOneDynamic(remoteDb, FETCH_TABLES_SIZES(s.databases[0], inParam))
-	MAX := 0.05
-	if *estimated > MAX {
-		log.Panicf("Error: total tables size %f GB exceeds %f GB", estimated, MAX)
-	} else {
-		fmt.Printf("Estimated replication size: %f", estimated)
-	}
-
-	return
 
 	// appenc foreign tables to table slice
-	for _, tableName := range inverseTopologicalOrdering {
-		s.tables = append(s.tables, tableName)
+	for _, table := range s.tables {
+		log.Printf("Replicating %s", table)
+		// get data to create template
+		createCommand := QueryForTwoColumns(remoteDb, SHOW_CREATE(s.databases[0], table))[0][1]
+		columns := QueryForTwoColumns(remoteDb, SELECT_COLUMNS(table))
+
+		// log.Println(createCommand)
+		// log.Println(columns)
+		// form the select query that results in inserts
+		insertTemplate := CreateSelectInsertionFromSchema(s.databases[0], table, columns)
+		// get an insert for each row
+		inserts := QueryFor(remoteDb, insertTemplate)
+		Populate(localDb, s.databases[0], createCommand, inserts)
 	}
-	// for _, table := range s.tables {
-	// 	log.Printf("Replicating %s", table)
-	// 	// get data to create template
-	// 	createCommand := QueryForTwoColumns(remoteDb, SHOW_CREATE(s.databases[0], table))[0][1]
-	// 	columns := QueryForTwoColumns(remoteDb, SELECT_COLUMNS(table))
-	//
-	// 	// log.Println(createCommand)
-	// 	// log.Println(columns)
-	// 	// form the select query that results in inserts
-	// 	insertTemplate := CreateSelectInsertionFromSchema(s.databases[0], table, columns)
-	// 	// get an insert for each row
-	// 	inserts := QueryFor(remoteDb, insertTemplate)
-	// 	Populate(localDb, s.databases[0], createCommand, inserts)
-	// }
-	// // close db as were going to open it again in raw tcp form
-	// localDb.Close()
-	//
-	// // start proxying
-	// socket, err := net.Listen("tcp", "127.0.0.1:3307")
-	// if err != nil {
-	// 	log.Fatalf("failed to start proxy: %s", err.Error())
-	// }
-	// fmt.Printf("Listening on localhost:%d\n", 3307)
-	// for {
-	// 	originSocket, err := socket.Accept()
-	// 	if err != nil {
-	// 		log.Fatalf("failed to accept connection: %s", err.Error())
-	// 	}
-	// 	go handleConn(originSocket, s.databases[0], s.tables[0])
-	// }
-	//
+	// close db as were going to open it again in raw tcp form
+	localDb.Close()
+
+	// start proxying
+	socket, err := net.Listen("tcp", "127.0.0.1:3307")
+	if err != nil {
+		log.Fatalf("failed to start proxy: %s", err.Error())
+	}
+	fmt.Printf("Listening on localhost:%d\n", 3307)
+	for {
+		originSocket, err := socket.Accept()
+		if err != nil {
+			log.Fatalf("failed to accept connection: %s", err.Error())
+		}
+		go handleConn(originSocket, s.databases[0], s.tables[0])
+	}
+
 }
 
 const COM_QUERY = byte(0x03)
